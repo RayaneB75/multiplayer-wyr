c76396ba7a9e8a4e3d69d049b3177838631ca86f
Rayane Belkhir <rayan.belkhir@imt-atlantique.net>
2023-08-07 22:52:59 +0200

    fix: Typos and struct

--- api/src/Misc/auth.py
+++ api/src/Misc/auth.py
@@ -13,11 +13,9 @@ def check_token(http_request):
         * Param         : http request
     """
     # Token to be used in app
-    token_parser = os.getenv("PARSER_TOKEN")
-    token_app = os.getenv("FRONT_TOKEN")
-    token_ansible = os.getenv("ANSIBLE_TOKEN")
-    token_postman = os.getenv("POSTMAN_TOKEN")
-    token_cron = os.getenv("CRON_TOKEN")
+    token_parser = os.getenv("FRONT_TOKEN")
+    token_app = os.getenv("DASH_TOKEN")
+    token_ansible = os.getenv("ADMIN_TOKEN")
     token_len = 72
     prefix = "Bearer "
     info = "Token_used : "
@@ -28,29 +26,18 @@ def check_token(http_request):
         if bearer.startswith(prefix) and len(bearer) == (len(prefix) + token_len):
             bearer = bearer[len(prefix) :]
             if bearer == token_app:
-                info += "app"
+                info += "front"
                 logging.info(info)
-                return "app"
+                return "front"
             elif bearer == token_ansible:
-                info += "ansible"
+                info += "dashboard"
                 logging.info(info)
-                return "ansible"
+                return "dashboard"
             elif bearer == token_parser:
-                info += "parser"
+                info += "admin"
                 logging.info(info)
-                return "parser"
-            elif bearer == token_postman:
-                info += "postman"
-                logging.info(info)
-                return "postman"
-            elif bearer == token_cron:
-                info += "cron"
-                logging.info(info)
-                return "cron"
-            else:
-                info += "unknown : " + bearer
-                logging.info(info)
-                return None
+                return "admin"
+            return None
         else:
             msg = "Authorization: bearer prefix not valid"
             logging.warning(msg)
@@ -69,58 +56,30 @@ def check_authorisation(http_request):
         * Param         : http_request
     """
     # List of authorisation by application usage
-    list_app = [["/company/name", "GET"], ["/blog", "GET"]]
-    list_ansible = [["/company", "GET"], ["/company", "POST"], ["/company", "DELETE"]]
-    list_parser = [["/company/name", "GET"], ["/company/email", "GET"]]
-    list_postman = [
-        ["/company", "GET"],
-        ["/company/name", "GET"],
-        ["/company/email", "GET"],
-        ["/company", "POST"],
-        ["/company", "UPDATE"],
-        ["/company", "DELETE"],
-        ["/blog", "GET"],
-        ["/blog", "POST"],
-        ["/blog", "UPDATE"],
-        ["/blog", "DELETE"],
+    list_front = [
+        ["/register", "POST"],
+        ["/login", "POST"],
+        ["/match", "POST"],
+        ["/pull", "GET"],
+        ["/push", "POST"],
     ]
-    list_cron = [["/blog", "GET"], ["/blog/update", "GET"]]
+    list_admin = [list_front]
+    list_dashboard = [["/display/dashboard", "GET"], ["/display/scoreboard", "GET"]]
 
     # Get token name
     token_used = check_token(http_request)
 
     # Check if users is authorised by checking the path & method couple in lists
     if token_used is not None:
-        path = http_request.path
-        if path[0:5] == "/blog":
-            path_to_use = path[0:5]
-        elif path[0:8] == "/company" and http_request.method != "GET":
-            path_to_use = path[0:8]
-        elif path[0:8] == "/company" and http_request.method == "GET":
-            if len(path) == 8:
-                path_to_use = path
-            elif path[0:13] == "/company/name":
-                path_to_use = path[0:13]
-            elif path[0:14] == "/company/email":
-                path_to_use = path[0:14]
-            else:
-                path_to_use = None
-        else:
-            path_to_use = None
+        path_to_use = http_request.path
 
         list_to_test = [path_to_use, http_request.method]
 
-        if token_used == "app" and list_to_test in list_app:
+        if token_used == "front" and list_to_test in list_front:
             return True
-        elif token_used == "ansible" and list_to_test in list_ansible:
+        if token_used == "dashboard" and list_to_test in list_dashboard:
             return True
-        elif token_used == "parser" and list_to_test in list_parser:
+        if token_used == "admin" and list_to_test in list_admin:
             return True
-        elif token_used == "postman" and list_to_test in list_postman:
-            return True
-        elif token_used == "cron" and list_to_test in list_cron:
-            return True
-        else:
-            return False
-    else:
         return False
+    return False
