cc938b8a1e1b7d5b3f092dad72216af1db6afc35
Rayane Belkhir <rayan.belkhir@imt-atlantique.net>
2023-08-18 21:44:27 +0200

    fix: userId by clicking on button

--- front/lib/find_match.dart
+++ front/lib/find_match.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:frontend/wyr.dart';
 
 class FindMatchWindow extends StatelessWidget {
-  final String? token;
-  final int userId;
-  
+  final String token;
+  final String userId;
+
   const FindMatchWindow({super.key, required this.token, required this.userId});
 
   @override
@@ -18,98 +18,82 @@ class FindMatchWindow extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Column(
-          children:<Widget>[
-            FindMatchForm(userId: userId),
-            // more if necessary 
-          ]
-        )
-      ),
-    );
-  }
-}
-
-class FindMatchForm extends StatefulWidget {
-  final int userId;
-
-  const FindMatchForm({Key? key, required this.userId}) : super(key: key);
-
-  @override
-  State<FindMatchForm> createState() => _FindMatchFormState();
-
-}
-
-class _FindMatchFormState extends State<FindMatchForm> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        const Text(
-          'Trouve un partenaire !',
-          style: TextStyle(fontSize: 30),
-        ),
-        const SizedBox(height: 120),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Entre l\'identifiant du joueur',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: SizedBox(
-            height: 50,
-            width: 150,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const WyrWindow()),
-                );
-              },
-              child: const Text(
-                'Match !',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Column(children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Trouve un partenaire !',
+                style: TextStyle(fontSize: 30),
               ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 75),
-          child: ElevatedButton(
-            onPressed: () {
-              showModalBottomSheet<void>(
-                context: context,
-                builder: (BuildContext context) {
-                  return SizedBox(
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                         Text('$widget.userId'),
-                          // ElevatedButton(
-                          //   child: const Text('Close BottomSheet'),
-                          //   onPressed: () => Navigator.pop(context),
-                          // ),
-                        ],
-                      ),
+              const SizedBox(height: 120),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Entre l\'identifiant du joueur',
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: SizedBox(
+                  height: 50,
+                  width: 150,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const WyrWindow()),
+                      );
+                    },
+                    child: const Text(
+                      'Match !',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  );
-                },
-              );
-            },
-            child: const Text('Mon identifiant'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 75),
+                child: ElevatedButton(
+                  onPressed: () {
+                    showModalBottomSheet<void>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SizedBox(
+                          height: 200,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(userId),
+                                // ElevatedButton(
+                                //   child: const Text('Close BottomSheet'),
+                                //   onPressed: () => Navigator.pop(context),
+                                // ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: const Text('Mon identifiant'),
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
+        ]),
+      ),
     );
   }
 }
