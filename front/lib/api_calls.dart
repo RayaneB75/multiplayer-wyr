a0db1645b6e6dcfee8a670565ca4f27e2e863122
Rayane Belkhir <rayan.belkhir@imt-atlantique.net>
2023-08-18 18:38:51 +0200

    fix(login): To be tested

--- front/lib/api_calls.dart
+++ front/lib/api_calls.dart
@@ -1,19 +1,21 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
-class ApiCalls {
+import 'package:frontend/findMatch.dart';
 
+class ApiCalls {
   static String? protocol = dotenv.env['API_SRV_PROTOCOL'];
   static String? domain = dotenv.env['API_SRV_HOSTNAME'];
   static String? port = dotenv.env['API_SRV_PORT'];
 
   static String token = "";
-  static String refreshToken= "";
+  static String refreshToken = "";
 
-  static String lastError ="";
+  static String lastError = "";
 
   // opensession endpoint management
   static Future openSession() async {
@@ -31,45 +33,60 @@ class ApiCalls {
     );
 
     if (response.statusCode == 200) {
-
       final json = jsonDecode(response.body);
 
       token = json['token'];
       refreshToken = json['refresh_token'];
-
     } else {
       throw Exception('Failed to open session');
     }
-
   }
 
 // login endpoint management
-  static Future login(String email, String password) async {
-    
+  static Future login(
+      String email, String password, BuildContext context) async {
     const String endpoint = "login";
     int result = 0;
 
-    await http.post(
-        Uri.parse('$protocol://$domain:$port/$endpoint'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          HttpHeaders.authorizationHeader: "Bearer $token",
-        },
-        body: jsonEncode(<String, String>{
-          "email": email,
-          "password": password,
-        }),
-      ).then((response) => {
-        if (response.statusCode == 200) {
-          result = 200;
-        } else {
-          //jsonDecode(response.body);
-          result = response.statusCode;
-        }
-      });
+    await http
+        .post(
+          Uri.parse('$protocol://$domain:$port/$endpoint'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            HttpHeaders.authorizationHeader: "Bearer $token",
+          },
+          body: jsonEncode(<String, String>{
+            "email": email,
+            "password": password,
+          }),
+        )
+        .then((response) => {
+              if (response.statusCode == 200)
+                {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FindMatchWindow()),
+                  )
+                }
+              else
+                {
+                  showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                      title: const Text('Erreur'),
+                      content: Text(jsonDecode(response.body)),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'OK'),
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    ),
+                  )
+                }
+            });
 
     return result;
   }
-
 }
-
--- front/lib/login.dart
+++ front/lib/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/api_calls.dart';
-import 'package:frontend/findMatch.dart';
 import 'package:frontend/items.dart';
 
 class LoginWindow extends StatelessWidget {
@@ -14,12 +13,9 @@ class LoginWindow extends StatelessWidget {
           'assets/logo_resel.png',
           fit: BoxFit.contain,
           height: 32,
-          
         ),
       ),
-      body: const Center(
-        child: LoginForm()
-      ),
+      body: const Center(child: LoginForm()),
     );
   }
 }
@@ -32,12 +28,10 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-
   final _formKey = GlobalKey<FormState>();
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-
 
   @override
   void dispose() {
@@ -48,14 +42,12 @@ class _LoginFormState extends State<LoginForm> {
     super.dispose();
   }
 
-  Future login(String email, String password) async {
-    return await ApiCalls.login(email, password);
+  Future login(String email, String password, context) async {
+    return await ApiCalls.login(email, password, context);
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     return Form(
       key: _formKey,
       child: Column(
@@ -63,23 +55,19 @@ class _LoginFormState extends State<LoginForm> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-    
           const Text(
             'Se connecter',
             style: TextStyle(fontSize: 30),
           ),
-    
           const SizedBox(height: 120),
-    
           TextFieldCustom(
             content: "Entre ton e-mail IMT Atlantique",
             controller: emailController,
-            ),
+          ),
           PasswordFieldCustom(
             content: "Entre ton mot de passe",
             controller: passwordController,
-            ),
-    
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             child: ElevatedButton(
@@ -93,27 +81,8 @@ class _LoginFormState extends State<LoginForm> {
                 if (isFrontValid) {
                   // call login api
                   // ignore: unrelated_type_equality_checks
-                  if (login(emailController.text, passwordController.text) == 200) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const FindMatchWindow()),
-                    );
-                  } else {
-                    showDialog<String> (
-                      context: context,
-                      builder: (BuildContext context) => AlertDialog(
-                        title: const Text('Error'),
-                        content: Text(ApiCalls.lastError),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, 'OK'),
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                } 
+                  login(emailController.text, passwordController.text, context);
+                }
               },
               child: const Text('Connexion'),
             ),
